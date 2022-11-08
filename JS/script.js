c2567882d64ea86db2ac0ad7268a5c7e2de8895a
O'lmasjon Yuldoshev <olmasjonyuldoshev@gmail.com>
2022-11-08 21:17:58 +0500

    Comments were written to the JavaScript file

--- JS/script.js
+++ JS/script.js
@@ -15,7 +15,7 @@
 //     }
 // });
 
-
+// Select element function
 function select(el) {
     if(el)
         if (el.trim()) return document.querySelector(el)
@@ -26,6 +26,7 @@ function selectAll(el) {
         if (el.trim()) return document.querySelectorAll(el) 
         else console.log('Please enter element...!')
 }
+// Import elements from html
 const body  = select('body')
 const nav = select('#nav-mobile')
 const toggleBtn = select('#toggle-menu')
@@ -36,22 +37,23 @@ const bookingContent = select('.booking-content')
 const addRoomBtn = select('.add-room')
 const roomContainer = selectAll('.room-container')
 
-document.addEventListener('click', (e) => {
-    if (e.target.nodeName == 'SELECT') {
-        const inputContainer = e.target.parentElement
-        inputContainer.children[1].classList.toggle('select-active')
-    } else {
-        selectDown.forEach((down) => {
-            if (down.classList.contains('select-active')) {
-                down.classList.remove('select-active')
-            }
-        })
-    }
-    
-})
-
+// 'Select' of the form events function
+function selectFunc(e) {
+    document.addEventListener('click', (e) => {
+        if (e.target.nodeName == 'SELECT') {
+            const inputContainer = e.target.parentElement
+            inputContainer.children[1].classList.toggle('select-active')
+        } else {
+            selectDown.forEach((down) => {
+                if (down.classList.contains('select-active')) {
+                    down.classList.remove('select-active')
+                }
+            })
+        }
+    })
+}
+// Show menu function
 function showMenu() {
-    
     toggleBtn.classList.add('d-none')
     xBtn.classList.remove('d-none')
     body.style.overflow = 'hidden'
@@ -61,6 +63,7 @@ function showMenu() {
     }, 10)
     nav.style.transform = 'translateY(0)'
 }
+// Close menu function
 function closeMenu() {
     xBtn.classList.add('d-none')
     toggleBtn.classList.remove('d-none')
@@ -71,16 +74,21 @@ function closeMenu() {
         nav.classList.add('d-none')
     }, 300)
 }
+// SelectFunc call
+selectFunc()
 
+// Show menu event
 toggleBtn.addEventListener('click', (e) => {
     showMenu()
 })
+// Close menu event
 xBtn.addEventListener('click', (e) => {
     closeMenu()
 })
-
+// Room counter
 let counterRoom = 2
 
+// Add room html element function
 function addRoom() {
     select('.room-num').parentElement.classList.remove('d-none')
     const newRoom = `
@@ -132,56 +140,14 @@ function addRoom() {
     </div>`
     bookingContent.innerHTML += newRoom
 }
-
+// Remove room html element
 function removeRoom(e) {
     const target = e.target.parentElement.parentElement.parentElement.parentElement.parentElement
-    // e.target.parentElement.parentElement.parentElement.parentElement.parentElement.remove()
     target.remove()
-    // console.log(target);
 }
-
-
-
-if (addRoomBtn)
-    addRoomBtn.addEventListener('click', (e) => {
-        addRoom()
-    })
-
-setInterval(() => {
-    if (selectAll('.room-container').length > 1) {
-        select('.del-btn').addEventListener('click', (e) => {
-            removeRoom(e)
-        })
-    } else if (selectAll('.room-container').length >= 1) {
-        counterRoom = 2
-        select('.room-num').parentElement.classList.add('d-none')
-    }
-}, 500)
-
-
-if (select('.btn-guests')) {
-    select('.btn-guests').addEventListener('click', (e) => {
-        select('.bron-dropdown-wrapper').classList.toggle('d-none')
-    })
-}
-if (select('.done')) {
-    select('.done').addEventListener('click', (e) => {
-        e.preventDefault()
-        select('.bron-dropdown-wrapper').classList.add('d-none')
-    })
-}
-if (select('.form-bron')) {
-    select('.form.bron').addEventListener('submit', (e) => {
-        selectAll('.bron-value').forEach((item) => {
-            let result = 0
-            result += item
-            console.log(result);
-        })
-    })
-}
-if (select('.btn-white')) {
-    select('.btn-white').addEventListener('click', (e) => {
-        e.preventDefault()
+// Dropdown showing the number of guests
+function guestsShow(e) {
+    e.preventDefault()
         if (select('.dropdown-wrapper__rooms').children.length < 5) {
             select('.dropdown-wrapper__rooms').innerHTML += (`
             <div class="dropdown-wrapper__room col-12">
@@ -213,25 +179,58 @@ if (select('.btn-white')) {
             </div>
             `) 
         }
+}
+// Add room btn checker
+if (addRoomBtn)
+    addRoomBtn.addEventListener('click', (e) => {
+        addRoom()
+    })
+// Remove room function call checker 500ms
+setInterval(() => {
+    // If the length of 'room-container' is
+    // greater than 1, 'del-btn' is clicked and the room is deleted
+    if (selectAll('.room-container').length > 1) {
+        select('.del-btn').addEventListener('click', (e) => {
+            removeRoom(e)
+        })
+    } else if (selectAll('.room-container').length >= 1) {
+        counterRoom = 2
+        select('.room-num').parentElement.classList.add('d-none')
+    }
+}, 500)
+// bron dropdown wrapper show or hide checker
+if (select('.btn-guests')) {
+    select('.btn-guests').addEventListener('click', (e) => {
+        select('.bron-dropdown-wrapper').classList.toggle('d-none')
     })
 }
-
-// setInterval(() => {
-//     if (select('.fa-trash-can')) {
-//         select('.fa-trash-can').addEventListener('click', (e) => {
-//             const target = e.target.parentElement.parentElement.parentElement
-//             target.remove()
-//             counterRoom--
-//         })
-//     }
-
-// }, 500)
-
+if (select('.done')) {
+    select('.done').addEventListener('click', (e) => {
+        e.preventDefault()
+        select('.bron-dropdown-wrapper').classList.add('d-none')
+    })
+}
+if (select('.form-bron')) {
+    select('.form.bron').addEventListener('submit', (e) => {
+        selectAll('.bron-value').forEach((item) => {
+            let result = 0
+            result += item
+        })
+    })
+}
+// Guests show function checking and calling
+if (select('.btn-white')) {
+    select('.btn-white').addEventListener('click', (e) => {
+        guestsShow(e)
+    })
+}
+// Check length of ".dropdown-wrapper__room" every 500ms, if greater than 1,
+// remove room from HTML, if length is less than or equal to 1,
+// set room counter to 2
 setInterval(() => {
     if (selectAll('.dropdown-wrapper__room').length > 1) {
         select('.fa-trash-can').addEventListener('click', (e) => {
             const target = e.target.parentElement.parentElement.parentElement
-            // target.style.
             target.style.transform = 'translateX(-400px)'
             target.style.backgroundColor = 'rgb(255, 211, 211)'
             setTimeout(() => {
@@ -244,45 +243,34 @@ setInterval(() => {
     }
 }, 500)
 
-// Rooms carousel
+// Rooms carousel function
 function caruselFunc(carousel, arrowIcons) {
-    // const firstImg = selectAll(".corousel-img")[0].clientWidth +20
     let isDragStart = false, isDragging = false, prevPageX, prevScrollLeft, positionDiff;
-
     const showHideIcons = () => {
         let scrollWidth = carousel.scrollWidth - carousel.clientWidth;
         arrowIcons[0].style.display = carousel.scrollLeft === 0 ? "none" : "block";
         arrowIcons[1].style.display = carousel.scrollLeft - scrollWidth > -1 ? "none" : "block";
     }
-
     arrowIcons.forEach(icon => {
-        // let selectAll(".corousel-img")[0].clientWidth +20 = firstImg.clientWidth + 20;
-        // console.log(firstImg.clientWidth);
         icon.addEventListener("click", () => {
             carousel.scrollLeft += icon.classList.contains('left') ? -selectAll(".corousel-img")[0].clientWidth +20 : selectAll(".corousel-img")[0].clientWidth +20;
             setTimeout(() => showHideIcons(), 60);
         });
     });
-
     const autoSlide = () => {
         if(carousel.scrollLeft - (carousel.scrollWidth - carousel.clientWidth) > -1 || carousel.scrollLeft <= 0) return;
-        
         positionDiff = Math.abs(positionDiff);
-        // let selectAll(".corousel-img")[0].clientWidth +20 = firstImg.clientWidth + 14;
         let valDifference = selectAll(".corousel-img")[0].clientWidth +20 - positionDiff;
-
         if(carousel.scrollLeft > prevScrollLeft) {
             return carousel.scrollLeft += positionDiff > selectAll(".corousel-img")[0].clientWidth +20 / 4 ? valDifference : -positionDiff;
         }
         carousel.scrollLeft -= positionDiff > selectAll(".corousel-img")[0].clientWidth +20 / 4 ? valDifference : -positionDiff;
     }
-
     const dragStart = (e) => {
         isDragStart = true;
         prevPageX = e.pageX || e.touches[0].pageX;
         prevScrollLeft = carousel.scrollLeft;
     }
-
     const dragging = (e) => {
         if(!isDragStart) return;
         isDragging = true;
@@ -292,7 +280,6 @@ function caruselFunc(carousel, arrowIcons) {
         carousel.scrollLeft = prevScrollLeft - positionDiff;
         showHideIcons();
     }
-
     const dragStop = () => {
         isDragStart = false;
         carousel.classList.remove("dragging");
@@ -301,7 +288,7 @@ function caruselFunc(carousel, arrowIcons) {
         isDragging = false;
         autoSlide();
     }
-
+    // Throw an event if the carousel function is present
     if(carousel) {
         carousel.addEventListener("mousedown", dragStart);
         carousel.addEventListener("touchstart", dragStart);
@@ -315,17 +302,15 @@ function caruselFunc(carousel, arrowIcons) {
     }
     
 }
-
+// Call the carousel function
 caruselFunc(select('.carousel-one'), selectAll('.carousel-one-icon'))
 caruselFunc(select('.carousel-two'), selectAll('.carousel-two-icon'))
 caruselFunc(select('.carousel-three'), selectAll('.carousel-three-icon'))
 caruselFunc(select('.carousel-four'), selectAll('.carousel-four-icon'))
 caruselFunc(select('.carousel-five'), selectAll('.carousel-five-icon'))
-
 caruselFunc(select('.carousel-one-facilities'), selectAll('.carousel-one-icon-facilities'))
 caruselFunc(select('.carousel-two-facilities'), selectAll('.carousel-two-icon-facilities'))
 caruselFunc(select('.carousel-three-facilities'), selectAll('.carousel-three-icon-facilities'))
 caruselFunc(select('.carousel-four-facilities'), selectAll('.carousel-four-icon-facilities'))
 caruselFunc(select('.carousel-five-facilities'), selectAll('.carousel-five-icon-facilities'))
 caruselFunc(select('.carousel-six-facilities'), selectAll('.carousel-six-icon-facilities'))
-
